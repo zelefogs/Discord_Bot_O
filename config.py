663a7147b329a179ff2a31f7f466773d7608104b
zelefogs <zelefogs@gmail.com>
2020-10-31 17:15:29 +0300

    1

--- config.py
+++ config.py
@@ -2,11 +2,10 @@ import os
 
 
 settings = {
-    'token': 'NzcxNDkwNjUzNzk1OTA5Njc0.X5s4qQ.NjREzdVbKsN8gokGnI25Q9J8z7M',
+    'token': '',
     'bot': 'test_bot',
     'id': 771490653795909674,
     'prefix': '<'
 }
 
-OWM_API_KEY = "a6ba6f29f966dbd628a539b3f2b14f29"
-GROUPS_SUBS = os.environ.get("GROUPS_SUBS", "Insert your groups separated by commas")
+OWM_API_KEY = ""
