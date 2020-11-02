a00db62cd2782fc98bc110412acfef6f5894163d
zelefogs <zelefogs@gmail.com>
2020-11-02 14:06:39 +0300

    make it better

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 settings = {
-    'token': os.environ.get("token", "Insert your API key OWM here"),
+    'token': (os.environ.get("token", "Insert your API key OWM here")),
     'bot': 'test_bot',
     'id': 771490653795909674,
     'prefix': '<'
